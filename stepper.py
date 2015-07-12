91041d520088ce70b5e19cf19bca0ec96dae325a
root <root@raspberrypi.(none)>
2015-07-12 13:07:23 +0200

    Keypress control

--- stepper.py
+++ stepper.py
@@ -1,28 +1,30 @@
-#!/usr/bin/python
-#--------------------------------------
-#    ___  ___  _ ____          
-#   / _ \/ _ \(_) __/__  __ __ 
-#  / , _/ ___/ /\ \/ _ \/ // / 
-# /_/|_/_/  /_/___/ .__/\_, /  
-#                /_/   /___/   
-#
-#    Stepper Motor Test
-#
-# A simple script to control
-# a stepper motor.
-#
-# Author : Matt Hawkins
-# Date   : 20/03/2015
-#
-# http://www.raspberrypi-spy.co.uk/
-#
-#--------------------------------------
-#!/usr/bin/env python
-
-# Import required libraries
 import sys
 import time
 import RPi.GPIO as GPIO
+import tty, termios
+import threading
+
+
+
+def getchar():
+  #Returns a single character from standard input
+  fd = sys.stdin.fileno()
+  old_settings = termios.tcgetattr(fd)
+  try:
+    tty.setraw(sys.stdin.fileno())
+    ch = sys.stdin.read(1)
+  finally:
+    termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+  
+  global read_key
+  read_key = ch
+    
+  if ch !='x':
+    #print ord(ch)
+    threading.Thread(target = getchar).start()
+
+
+
 
 # Use BCM GPIO references
 # instead of physical pin numbers
@@ -31,10 +33,10 @@ GPIO.setmode(GPIO.BCM)
 # Define GPIO signals to use
 # Physical pins 11,15,16,18
 # GPIO17,GPIO22,GPIO23,GPIO24
-if sys.argv[2]=='l':
-	StepPins = [4,17,27,22]
-else:
-	StepPins = [18,23,24,25]
+#if sys.argv[2]=='l':
+StepPins = [4,17,27,22]
+#else:
+#StepPins = [18,23,24,25]
 
 # Set all pins as output
 for pin in StepPins:
@@ -44,6 +46,7 @@ for pin in StepPins:
 
 # Define advanced sequence
 # as shown in manufacturers datasheet
+
 Seq = [[1,0,0,0],
        [1,1,0,0],
        [0,1,0,0],
@@ -54,32 +57,64 @@ Seq = [[1,0,0,0],
        [1,0,0,1]
        
 ]
+"""
+Seq = [[1,0,0,0],
+       [1,0,0,1],
+       [0,0,0,1],
+       [0,0,1,1],
+       [0,0,1,0],
+       [0,1,1,0],
+       [0,1,0,0],
+       [1,1,0,0]
        
+]
+"""
+
 StepCount = len(Seq)-1
-if sys.argv[3]=='b':
-	StepDir = -1 # Set to 1 or 2 for clockwise
+#if sys.argv[3]=='b':
+#	StepDir = -1 # Set to 1 or 2 for clockwise
             # Set to -1 or -2 for anti-clockwise
-else:
-	StepDir = 1
+#else:
+StepDir = 1
 
 # Read wait time from command line
 if len(sys.argv)>1:
   WaitTime = int(sys.argv[1])/float(1000)
 else:
-  WaitTime = 10/float(1000)
+  WaitTime = 1/float(1000)
 
 # Initialise variables
 StepCounter = 0
 
+
+
+
 # Start main loop
+global read_key
+read_key = ''
+
+threading.Thread(target = getchar).start()
+print "Press x to exit!"
 while True:
+  
+  if read_key == 'x':
+      GPIO.cleanup()
+      exit(1)
+  elif read_key == chr(65):
+    ''' UP '''
+    StepDir = 1
+  elif read_key == chr(66):
+    ''' Down '''
+    StepDir = -1
+
+
 
   for pin in range(0, 4):
     xpin = StepPins[pin]
-    print StepCounter
-    print pin
+    #print StepCounter
+    #print pin
     if Seq[StepCounter][pin]!=0:
-      print " Step %i Enable %i" %(StepCounter,xpin)
+      #print " Step %i Enable %i" %(StepCounter,xpin)
       GPIO.output(xpin, True)
     else:
       GPIO.output(xpin, False)
