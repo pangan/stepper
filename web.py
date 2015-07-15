b8723f317f41ff5f3bf0407607a286ee7836957a
Amir <amir.mofakhar@birdstep.com>
2015-07-15 14:38:10 +0200

    adding default web

--- web.py
+++ web.py
@@ -1,1 +1,51 @@
 from lib import WebServer
+import sys
+import datetime 
+
+def time_stamp():
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+def send_command(cmd):
+	f = open('/tmp/m_command.txt','w')
+	f.write(cmd)
+	f.close()
+
+
+def getweb():
+
+  data_dic = webif.read()
+    
+  if data_dic:
+
+    if 'c' in data_dic:
+      if data_dic['c'] == 'forward':
+        print >>sys.stderr, '[%s] FORWARD   ' %time_stamp(), webif.client
+        send_command("FORWARD")
+        
+      if data_dic['c'] == 'reverce':
+        print >>sys.stderr, '[%s] REVERCE   ' %time_stamp(), webif.client
+        send_command("REVERCE")
+        
+      if data_dic['c'] == 'right':
+        print >>sys.stderr, '[%s] RIGHT     ' %time_stamp(), webif.client
+        send_command("RIGHT")
+        
+      if data_dic['c'] == 'left':
+        print >>sys.stderr, '[%s] LEFT      ' %time_stamp(), webif.client
+        send_command("LEFT")
+        
+      if data_dic['c'] == 'startstop':
+        print >>sys.stderr, '[%s] START/STOP' %time_stamp(), webif.client
+        send_command("STARTSTOP")
+        
+    if 'x' in data_dic:
+      print >>sys.stderr, '[%s] EXIT' %time_stamp(), webif.client
+      webif.close()
+      
+    
+
+
+webif = WebServer(port=8080,template='template.html')
+print "Press Ctrl+c to exit!"
+while True:
+	getweb()
